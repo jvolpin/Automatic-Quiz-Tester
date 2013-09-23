295607ca85469176c41b0f79518623a787d95cc2
Juan Pablo Volpintesta <juan.volpintesta@imagini.net>
2013-09-23 17:17:37 +0100

    initial commit

---
+++ src/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jvolpintesta
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        PiterFrame piter = new PiterFrame();
+        piter.setVisible(true);
+    }
+}
